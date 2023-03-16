2060af6e596b72ac193f4ccfd3da5d9640c2f8ad
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-03-16 18:04:56 +0800

    ui updates

--- src/js/app.js
+++ src/js/app.js
@@ -5,17 +5,19 @@ document.addEventListener('turbo:load', () => {
     // Get the header element
     let header = document.querySelector(".header");
 
-    // Add an event listener to the window for when it is scrolled
-    window.addEventListener('scroll', function() {
-        // Check if the page has been scrolled more than 20 pixels
-        if (window.scrollY > 20) {
-            // Add the "sticky" class to the header
-            header.classList.add("sticky");
-        } else {
-            // Remove the "sticky" class from the header
-            header.classList.remove("sticky");
-        }
-    });
+    if (header) {
+        // Add an event listener to the window for when it is scrolled
+        window.addEventListener('scroll', function() {
+            // Check if the page has been scrolled more than 20 pixels
+            if (window.scrollY > 20) {
+                // Add the "sticky" class to the header
+                header.classList.add("sticky");
+            } else {
+                // Remove the "sticky" class from the header
+                header.classList.remove("sticky");
+            }
+        });
+    }
 
     // Dropdown
     const toggleDropdown = (dropdown) => {
